8ea81bcc5ec86393ddf8c5b80a76d2b47894e17e
tehreem2688 <tehreemm121@gmail.com>
2025-04-08 02:41:08 +0500

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import re
+
+st.set_page_config(page_title="password strength checker", page_icon="🔐")
+
+st.title("password strenght checker")
+st.markdown("""welcome to the ultimate 
+password strength checker!
+use this simple tool to check the strengthof your password and get suggestion on how to make it stronger
+we will give you helpful tips to create a **strong password**
+""")
